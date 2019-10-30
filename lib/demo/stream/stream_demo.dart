1a772df275b4bfd551978c0df9e11dd3c65f76bd
Mark He <heke@hujiang.com>
2019-10-30 16:48:41 +0800

    移除重复测试demo

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -15,89 +15,6 @@ class StreamDemo extends StatelessWidget {
   }
 }
 
-class StreamDemoHome extends StatefulWidget {
-  @override
-  _StreamDemoHomeState createState() => _StreamDemoHomeState();
-}
-
-class _StreamDemoHomeState extends State<StreamDemoHome> {
-  //声明一个_streamSubscription 作为订阅的接收者
-  StreamSubscription _streamSubscription;
-
-
-  @override
-  void initState() {
-    super.initState();
-    //输出 创建stream log
-    debugPrint('create stream');
-    //通过Stream.fromFuture 构造一个字符串的流
-    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
-    //输出 开始监听
-    debugPrint('start listening stream');
-    //监听多个事件
-    _streamSubscription = _streamDemo.listen(onData,onError:onError,onDone:onDone);
-    debugPrint('Initialize completed.');
-  }
-
-  void onData(String data){
-    debugPrint('$data');
-  }
-
-  void onError(error){
-    debugPrint('error:$error');
-  }
-
-  void onDone(){
-    debugPrint('Done');
-  }
-
-  _pauseStream(){
-    debugPrint('pauseStream');
-    _streamSubscription.pause();
-  }
-
-  _resumeStream(){
-    debugPrint('resumeStream');
-    _streamSubscription.resume();
-  }
-
-  _cancelStream(){
-    debugPrint('cancelStream');
-    _streamSubscription.cancel();
-  }
-
-  Future<String> fetchData() async{
-    await Future.delayed(Duration(seconds: 6));
-    //throw 'something happened';
-    return 'hello~';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text('Pause'),
-              onPressed: _pauseStream,
-            ),
-            FlatButton(
-              child: Text('Resume'),
-              onPressed: _resumeStream,
-            ),
-            FlatButton(
-              child: Text('cancel'),
-              onPressed: _cancelStream,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 //use controller
 class StreamControllerDemo extends StatefulWidget {
   @override
@@ -108,8 +25,6 @@ class _StreamControllerDemoState extends State<StreamControllerDemo> {
   StreamController<String> _streamDemo;
   StreamSubscription _streamSubscription;
   StreamSink<String> _streamSink;
-
-
 
   @override
   void initState() {
